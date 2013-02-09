bdf8485f9e39c0f576022ec72b1ceb51fdd37c39
emanuele <emanuele45@gmail.com>
2013-02-09 14:49:26 +0100

    Initial commit

---
+++ Subs-Customize.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Customize SMF
+ *
+ * @package Customize SMF
+ * @author emanuele
+ * @copyright 2013 emanuele, Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 0.0.1
+ */
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+function custsmf_add_menu (&$profile_areas)
+{
+// qui metteremo un po' di codice
+}
---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Customize SMF
+ *
+ * @package Customize SMF
+ * @author emanuele
+ * @copyright 2013 emanuele, Simple Machines
+ * @license http://www.simplemachines.org/about/smf/license.php BSD
+ *
+ * @version 0.0.1
+ */
+
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+elseif (!defined('SMF'))
+	exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+
+remove_integration_hook('integrate_profile_areas', 'custsmf_add_menu');
+remove_integration_hook('integrate_pre_include', '$sourcedir/Subs-Customize.php');
